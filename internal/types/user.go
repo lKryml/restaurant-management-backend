5b858be48bfe06fe5485c606d68277c0bad98461
lKryml <gumimash@gmail.com>
2024-09-07 14:03:47 +0200

    update user model

--- internal/types/user.go
+++ internal/types/user.go
@@ -5,10 +5,10 @@ import "github.com/google/uuid"
 type User struct {
 	ID         uuid.UUID `db:"id" json:"id"`
 	Name       string    `db:"name" json:"name"`
-	Img        string    `db:"img" json:"img"`
+	Img        *string   `db:"img" json:"img"`
 	Email      string    `db:"email" json:"email"`
 	Phone      string    `db:"phone" json:"phone"`
-	Password   string    `db:"password" json:"password"`
+	Password   string    `db:"password" json:"-"`
 	Created_at string    `db:"created_at" json:"created_at"`
 	Updated_at string    `db:"updated_at" json:"updated_at"`
 }
